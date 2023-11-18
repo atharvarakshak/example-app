325bdbcf921c70b4f8f5998918f226707a172d00
Atharva Rakshak <atharvarakshak23@gmail.com>
2023-11-18 14:42:35 +0530

    Add Practicals namespace as per PSR4 guidelines

--- practicals/song.php
+++ practicals/song.php
@@ -1,57 +1,88 @@
 <?php
-
 namespace Practicals;
 
-class Song
-{
-    // Properties
+class Song {
     private $title;
     private $artist;
-    private $duration;
+    private $genre;
+    private $tempo;
 
-    // Constructor
-    public function __construct($title, $artist, $duration)
-    {
+    /**
+     * Setter function for setting the title of the song
+     * @param string $title The title of the song
+     */
+    public function setTitle($title) {
         $this->title = $title;
-        $this->artist = $artist;
-        $this->duration = $duration;
     }
 
-    // Getter methods
-    public function getTitle()
-    {
+    /**
+     * Getter function for retrieving the title of the song
+     * @return string The title of the song
+     */
+    public function getTitle() {
         return $this->title;
     }
 
-    public function getArtist()
-    {
-        return $this->artist;
-    }
-
-    public function getDuration()
-    {
-        return $this->duration;
-    }
-
-    // Setter methods
-    public function setTitle($title)
-    {
-        $this->title = $title;
-    }
-
-    public function setArtist($artist)
-    {
+    /**
+     * Setter function for setting the artist of the song
+     * @param string $artist The artist of the song
+     */
+    public function setArtist($artist) {
         $this->artist = $artist;
     }
 
-    public function setDuration($duration)
-    {
-        $this->duration = $duration;
+    /**
+     * Getter function for retrieving the artist of the song
+     * @return string The artist of the song
+     */
+    public function getArtist() {
+        return $this->artist;
     }
 
-    // Other methods (you can add more as needed)
-    public function displayInfo()
-    {
-        echo "Title: {$this->getTitle()}, Artist: {$this->getArtist()}, Duration: {$this->getDuration()} minutes";
+    /**
+     * Setter function for setting the genre of the song
+     * @param string $genre The genre of the song
+     */
+    public function setGenre($genre) {
+        $this->genre = $genre;
+    }
+
+    /**
+     * Getter function for retrieving the genre of the song
+     * @return string The genre of the song
+     */
+    public function getGenre() {
+        return $this->genre;
+    }
+
+    /**
+     * Setter function for setting the tempo of the song
+     * @param string $tempo The tempo of the song
+     */
+    public function setTempo($tempo) {
+        $this->tempo = $tempo;
+    }
+
+    /**
+     * Getter function for retrieving the tempo of the song
+     * @return string The tempo of the song
+     */
+    public function getTempo() {
+        return $this->tempo;
     }
 }
+/*
+// Example usage:
+$song = new Song();
+
+$song->setTitle("Bohemian Rhapsody");
+$song->setArtist("Queen");
+$song->setGenre("Rock");
+$song->setTempo("70 BPM");
+
+echo "Title: " . $song->getTitle() . "\n";
+echo "Artist: " . $song->getArtist() . "\n";
+echo "Genre: " . $song->getGenre() . "\n";
+echo "Tempo: " . $song->getTempo() . "\n";
+*/
+?>
