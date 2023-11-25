f417f42733f2af0452a08501218a3f36a5a956f4
Atharva Rakshak <atharvarakshak23@gmail.com>
2023-11-25 15:23:51 +0530

    song test

--- app/View/Components/Layouts/Main.php
+++ app/View/Components/Layouts/Main.php
@@ -8,19 +8,34 @@ use Illuminate\View\Component;
 
 class Main extends Component
 {
+    //set title attribute 
+    public string $title = "Playlist App";
+    // set attribute for page name
+    public string $pageName;
+    // public string $pageName;
     /**
      * Create a new component instance.
      */
-    public function __construct()
-    {
-        //
-    }
+
+    public function __construct(string $pageName = '')
+     {
+        $this->pageName = $pageName;
+        
+      }
 
     /**
      * Get the view / contents that represent the component.
      */
     public function render(): View|Closure|string
     {
+        if($this->pageName)
+        {
+            $this->title = $this->title.' | '.$this->pageName;
+        }
         return view('components.layouts.main');
     }
+    
+    
+
+    
 }
